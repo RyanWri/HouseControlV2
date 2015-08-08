0615e303b5b3758134bed1032aac895eda67af34
Ryan Wright <ranman37@gmail.com>
2015-08-08 13:28:13 +0300

    Json Request to create room success + edit room + add device

--- WebContent/js/Room_Management.js
+++ WebContent/js/Room_Management.js
@@ -3,63 +3,54 @@
 	date 25/07/2015 
 	javascript for room Management page
 	Last Modification : 07/08/2015
-*/
+ */
 
-$(function() 
-{
-    
-    $("#formShowAllRooms").validate(
-    		{
-    			errorPlacement: function(error, element) 
-    		{
-    			error.insertAfter(element);
-    		} ,
-    		submitHandler: function(form) 
-    		{
-    			var data1 = $('#formShowAllRooms').serialize();
-    			$.mobile.loading("show");
-    			$.ajax({
-    				type: 'POST',
-    				url: '/HouseControl/api/devices_group/create',
-    				data: data1,
-    				success: function(result)
-    				{
-    					$.mobile.loading("hide");
-    					if (result.status === "error")
-    					{
-    						alert(result.data);
-    					}
-    					else
-    					{
-    						var roomName = $("#textBoxRoomName").val();
-    						alert (roomName);
-    					}
-    				},
-    				error: function()
-    				{
-    					$.mobile.loading("hide");
-    					alert("Connection Error");
-    				},
-    				statusCode: {
-    					400: function(){
-    						$.mobile.loading("hide");
-    						updateLabelLoginError("wrong username of password");
-    						setTimeout( $.mobile.hidePageLoadingMsg, 2000 );
-    					},
-    					401: function(){
-    						$.mobile.loading("hide");
-    						updateLabelLoginError("Unauthorized");
-    					},
-    					500: function(){
-    						$.mobile.loading("hide");
-    						updateLabelLoginError("already logged in");
-    					}
-    				}
-    			});
 
-    		}
-    	});
+$("#formShowAllRooms").submit( function()
+		{
+	var parameters = {};
+	var roomName = $('#textBoxRoomName').val();
+	parameters.name = roomName;
+	var parametersStringified = JSON.stringify(parameters);
+	$.mobile.loading("show");
+	$.ajax({
+		type: 'POST',
+		url: '/HouseControl/api/devices_group/create',
+		data: parametersStringified,
+		dataType: "json",
+		success: function(result)
+		{
+			appendRoom(roomName);
+			$.mobile.loading("hide");
+			alert(result.status);
+			
+		},
+		error: function(xhr, ajaxOptions, thrownError)
+		{
+			$.mobile.loading("hide");
+			alert(xhr.status);
+			alert(thrownError); 
+		}
+
+
+	});
+
+		}
+);
+
+
+/* refresh page and show all current rooms*/    
+$(document).on("pageload",function(){
+	$("#ListAllRooms").append("<b>Appended text</b>");
 });
 
-
+function appendRoom(roomName)
+{
+	var appendText = "<li id=\"" + roomName +"\"><h1>"+ roomName +"</h1></li>";
+	var addDeviceText = "<a id=\"" + roomName +"\" href=\"#EditRoom\" data-rel=\"popup\" class=\"ui-btn ui-btn-inline ui-corner-all\">"
+	addDeviceText += roomName + "</a>";
+	$("#ListAllRooms").append( appendText);
+	$("#ActualRooms").append( addDeviceText);
+	$("#EditRoomTitle").append(roomName);
+}
 
