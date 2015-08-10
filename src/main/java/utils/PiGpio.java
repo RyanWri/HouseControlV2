9a65c21eba0afcfa8969dc7299fedbb60ec50f58
igalzzz <Igal@Igal-PC>
2015-08-11 00:40:32 +0300

    PiGpio: updated entire file

--- src/main/java/utils/PiGpio.java
+++ src/main/java/utils/PiGpio.java
@@ -1,5 +1,11 @@
 package utils;
 
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.json.JSONObject;
+
 import com.pi4j.io.gpio.GpioController;
 import com.pi4j.io.gpio.GpioFactory;
 import com.pi4j.io.gpio.GpioPinDigitalOutput;
@@ -7,42 +13,61 @@ import com.pi4j.io.gpio.Pin;
 import com.pi4j.io.gpio.PinState;
 import com.pi4j.io.gpio.RaspiPin;
 
+import dataBases.jdbc.DeviceHandler;
+import dataBases.jdbc.DeviceUsageHandler;
 import dataBases.jdbc.RelayConnectionHandler;
+import modelObjects.Device;
 import modelObjects.RelayConnection;
+import modelObjects.RelayConnection.PortState;
 
-public class PiGpio{
+public class PiGpio {
 
-	private static GpioPinDigitalOutput myPins[];
+	
+	public static final int NUMBER_OF_PINS_IN_RPI = 29;
+	public static final String LOW = "LOW";
+	public static final String HIGH = "HIGH";
+	private static GpioPinDigitalOutput[] myPins;
 	private static GpioController gpio;
 
 	public static String controlGpioPin(int pinNumber , int action) throws Exception{
 		try{
-			if(pinNumber >=0 && pinNumber <=29 && action >=0 & action <=1)
+			if(pinNumber >=0 && pinNumber <NUMBER_OF_PINS_IN_RPI && action >=0 & action <=1)
 			{
 				if(myPins[pinNumber] != null && checkifActionConsistent(pinNumber ,action)){
-					myPins[pinNumber].toggle();					
+					RelayConnection[] relayConnectionsFromDB = RelayConnectionHandler.initRpiPinsFromDB();
+					if(relayConnectionsFromDB[pinNumber].getDevice().getDeviceID() > 0 ){
+						myPins[pinNumber].toggle();	
+						if(relayConnectionsFromDB[pinNumber].getState() == PortState.On){
+							RelayConnectionHandler.setDeviceOnOrOffInTheDB(relayConnectionsFromDB[pinNumber].getDevice().getDeviceID(), relayConnectionsFromDB[pinNumber].getRelayPort(), PortState.Off);
+							DeviceUsageHandler.updateDeviceUsageTurnOffTimeStamp(relayConnectionsFromDB[pinNumber].getDevice().getDeviceID(), DeviceUsageHandler.getCurrentTimeStamp());
+						}
+						else if(relayConnectionsFromDB[pinNumber].getState() == PortState.Off) {
+							RelayConnectionHandler.setDeviceOnOrOffInTheDB(relayConnectionsFromDB[pinNumber].getDevice().getDeviceID(), relayConnectionsFromDB[pinNumber].getRelayPort(), PortState.On);
+							DeviceUsageHandler.insertNewDeviceUsage(relayConnectionsFromDB[pinNumber].getDevice().getDeviceID(), DeviceUsageHandler.getCurrentTimeStamp());
+						}	
+					}
+					else{
+						throw new Exception("There is no device connected to this pin in the DB!");
+					}
 				}
 				else{
 					throw new Exception("The Action is not consistent with the device current status");
 				}
 			}
-
 			return getPinState(pinNumber);
 		}
 		catch(Exception ex) {
 			throw ex;
 		}
-
 	}
-
 
 	private static boolean checkifActionConsistent(int pinNumber, int action) throws Exception {
 		boolean actionConsistent = false;
 		try{
 			String currentPinState = getPinState(pinNumber);
-			if (currentPinState == "LOW" && action == 1)
+			if (currentPinState == LOW && action == 1)
 				actionConsistent = true;
-			else if ( currentPinState == "HIGH" && action == 0)
+			else if ( currentPinState == HIGH && action == 0)
 				actionConsistent = true ;
 			return actionConsistent;
 		}
@@ -58,28 +83,27 @@ public class PiGpio{
 				PinState currentPinState = myPins[pinNumber].getState();
 				return getPinStringState(currentPinState);	
 			}
-
 			else{
-				throw new Exception("Channel is not activated!");
+				throw new Exception("Port is not activated!");
 			}
 		}
 		catch (Exception ex){
 			throw ex;
 		}
-
 	}
 
 	private static String getPinStringState(PinState currentPinState) {
 
 		if (currentPinState == PinState.HIGH){
-			return "LOW";
+			return LOW;
 		}
 		else 
-			return "HIGH";
+			return HIGH;
 	}
 
-	public static void shutDownGpio() {
-		gpio.shutdown(); 
+	public static void shutDownGpio() throws Exception {
+		DeviceUsageHandler.turnOffAllTurnedOnDevicesInDB();
+		//gpio.shutdown();  ///???? DO I NEED TO SHUT EVERYTHING DOWN????
 	}
 
 	private static Pin getPinGPIO(int pinNumber) 
@@ -179,17 +203,88 @@ public class PiGpio{
 		}
 		return retPin;
 	}
+	
+	public static void initializePiGpio() throws Exception {
+		myPins = new GpioPinDigitalOutput [NUMBER_OF_PINS_IN_RPI];
+		
+		//gpio = GpioFactory.getInstance(); //$$ FOR DEBUG
 
-
-	public static void initializePiGpio() {
-		myPins = new GpioPinDigitalOutput[29]; 
-		gpio = GpioFactory.getInstance();
-
-		for (int pinNumber = 0; pinNumber < myPins.length; pinNumber++){
-			Pin gpioPin = getPinGPIO(pinNumber);
-			//SET HERE THE DB PINSTATE IN EACH myPins[pinNumber] 
-			myPins[pinNumber] = gpio.provisionDigitalOutputPin(gpioPin,PinState.HIGH);
+		try{
+			RelayConnection[] relayConnectionsFromDB = RelayConnectionHandler.initRpiPinsFromDB();
+			//doc: init to low ( bug in pi4j : HIGH is off and LOW is ON)		
+			for (int pinNumber = 0; pinNumber < NUMBER_OF_PINS_IN_RPI; pinNumber++){
+				Pin gpioPin = getPinGPIO(pinNumber);
+				if (relayConnectionsFromDB[pinNumber].getState() == PortState.On){
+					myPins[pinNumber] = gpio.provisionDigitalOutputPin(gpioPin);
+					myPins[pinNumber].setState(PinState.LOW);
+					DeviceUsageHandler.insertNewDeviceUsage(relayConnectionsFromDB[pinNumber].getDevice().getDeviceID(), DeviceUsageHandler.getCurrentTimeStamp());
+				}
+				else 
+					myPins[pinNumber] = gpio.provisionDigitalOutputPin(gpioPin,PinState.HIGH);
+			}
 		}
+		catch (Exception ex){
+			throw ex;
+		}
+	}
+
+
+	public static String getAllDevicesStatistics(String timeFrame) {
+		List<Device> devices = new ArrayList<Device>();
+		JSONObject deviceConsumption = new JSONObject();
+		double sum = 0;
+		Device currentDevice;
+		double hours;
+		
+		try{
+			if(timeFrame.equals("day") || timeFrame.equals("week") || timeFrame.equals("month"))
+			{
+				devices = DeviceHandler.getAllDevicesIDs();
+				for (Device device : devices) {
+					deviceConsumption = DeviceUsageHandler.getDeviceSumVoltageStatistics(device.getDeviceID(), timeFrame);
+					hours = (double) deviceConsumption.get("hours");
+					currentDevice = (Device) deviceConsumption.get("device");
+					sum += currentDevice.getVoltage() *((int) hours) + (currentDevice.getVoltage() * (((hours % 1d)/60d)*100d));
+				}
+			}
+			else{
+			throw new Exception("Please select day/week/month as a time frame!");
+			}
+			
+		}
+		
+		catch(Exception ex){
+			
+		}
+		return String.format("%.3f",sum);
+	}
+	
+	public static String getDeviceStatistics(String timeFrame, int deviceID) throws Exception {
+		String DeviceUsage = "";
+		JSONObject deviceConsumption = new JSONObject();
+		Device device;
+		double hours;
+		double voltage;
+		String totalConsumption;
+		try{
+			if(timeFrame.equals("day") || timeFrame.equals("week") || timeFrame.equals("month"))
+			{
+				deviceConsumption = DeviceUsageHandler.getDeviceSumVoltageStatistics(deviceID, timeFrame);
+				device = (Device) deviceConsumption.get("device");
+				hours = (double)deviceConsumption.get("hours");
+				voltage = (double)device.getVoltage();
+				totalConsumption = String.format("%.3f",voltage *((int) hours) + (voltage * (((hours % 1d)/60d)*100d)));
+				
+				DeviceUsage = "In the last "+ timeFrame + " device "+deviceID +":" + device.getName() + " consumed " + totalConsumption + " volts in "+ String.format("%.2f", hours) + " hours";
+			}
+			else{
+			throw new Exception("Please select day/week/month as a time frame!");
+			}			
+		}		
+		catch(Exception ex){
+			throw ex;
+		}
+		return DeviceUsage;
 	}
 
 }
