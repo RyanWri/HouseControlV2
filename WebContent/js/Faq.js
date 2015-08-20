47728e35cab4a9464466a448218be2fdc284140f
Ryan Wright <ranman37@gmail.com>
2015-08-20 11:54:50 +0300

    faq done

--- WebContent/js/Faq.js
+++ WebContent/js/Faq.js
@@ -1,4 +1,17 @@
-/**
- * 
+/*
+	Author: ran yamin
+	date 20/08/2015 
+	javascript for faq page
+	Last Modification : 
  */
 
+    $(document).ready(function()
+    {
+    	/*
+    	CreateAnswerNavigation();
+    	CreateAnswerDevices();
+    	CreateAnswerStatistics();
+    	CreateAnswerAdminRights();
+    	CreateAnswerTimer();
+    	CreateAnswerGeneral(); */
+    });  
