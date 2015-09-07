3aa9e4ba783f08d50ccfa5ca7b562e00fb9094ee
igalzzz <Igal@Igal-PC>
2015-09-07 20:50:13 +0300

    ApiDevice: updated reurn value of statistics/month/all

--- src/main/java/controllers/ApiDevice.java
+++ src/main/java/controllers/ApiDevice.java
@@ -220,8 +220,7 @@ public class ApiDevice{
 		try{
 			SessionHandler.verifyAuthenticatedUserRequest(req);
 			String sumAllDevicesStatistics = PiGpio.getAllDevicesStatistics(timeFrame);
-			response = Response.ok(GenericResponse.ok("During the last " + timeFrame + " is:   "
-					+ ""+ sumAllDevicesStatistics + "KW!")).build();
+			response = Response.ok(GenericResponse.ok(sumAllDevicesStatistics)).build();
 		}
 		catch(Exception ex){
 			response = Response.ok(GenericResponse.error(ex.getMessage())).build();
